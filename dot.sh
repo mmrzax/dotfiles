0d6cd4e188b6437f21e603a440820c6d46ca37e8
mmrzax <mmrzax@gmail.com>
2022-04-22 16:02:03 +0430

    feat: add dot.sh script for copying dotfiles

---
+++ dot.sh
@@ -0,0 +1,9 @@
+cp -r ~/.config/alacritty ~/Documents/dotfiles/.config;
+cp -r ~/.config/htop ~/Documents/dotfiles/.config;
+cp -r ~/.config/i3 ~/Documents/dotfiles/.config;
+cp -r ~/.config/i3status ~/Documents/dotfiles/.config;
+cp -r ~/.config/nitrogen ~/Documents/dotfiles/.config;
+cp -r ~/.config/nvim ~/Documents/dotfiles/.config;
+cp -r ~/.config/rofi ~/Documents/dotfiles/.config;
+cp ~/.tmux.conf ~/Documents/dotfiles;
+cp ~/.zshrc ~/Documents/dotfiles;
