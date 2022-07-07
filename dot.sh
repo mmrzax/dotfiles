05e75cd12921401cd505db273946548e260c6804
mmrzax <mmrzax@gmail.com>
2022-07-07 23:01:36 +0430

    feat: update script

--- dot.sh
+++ dot.sh
@@ -1,9 +1,9 @@
-cp -r ~/.config/alacritty ~/Documents/dotfiles/.config;
-cp -r ~/.config/htop ~/Documents/dotfiles/.config;
-cp -r ~/.config/i3 ~/Documents/dotfiles/.config;
-cp -r ~/.config/nvim ~/Documents/dotfiles/.config;
-cp -r ~/.config/rofi ~/Documents/dotfiles/.config;
-cp -r ~/.config/kitty ~/Documents/dotfiles/.config;
+cp -r ~/.config/alacritty ~/Documents/dotfiles
+cp -r ~/.config/htop ~/Documents/dotfiles
+cp -r ~/.config/i3 ~/Documents/dotfiles
+cp -r ~/.config/nvim ~/Documents/dotfiles
+cp -r ~/.config/rofi ~/Documents/dotfiles
+cp -r ~/.config/kitty ~/Documents/dotfiles
 cp ~/.tmux.conf ~/Documents/dotfiles;
 cp ~/.zshrc ~/Documents/dotfiles;
 cp ~/.zprofile ~/Documents/dotfiles;
