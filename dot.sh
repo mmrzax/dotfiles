03d887f71a731a59f0a016e4aced10a38cbf29e5
mmrzax <mmrzax@gmail.com>
2022-07-08 13:50:44 +0430

    feat: update i3 colnfig

--- dot.sh
+++ dot.sh
@@ -1,6 +1,7 @@
 cp -r ~/.config/alacritty ~/Documents/dotfiles
 cp -r ~/.config/htop ~/Documents/dotfiles
 cp -r ~/.config/i3 ~/Documents/dotfiles
+cp -r ~/.config/i3status ~/Documents/dotfiles
 cp -r ~/.config/nvim ~/Documents/dotfiles
 cp -r ~/.config/rofi ~/Documents/dotfiles
 cp -r ~/.config/kitty ~/Documents/dotfiles
