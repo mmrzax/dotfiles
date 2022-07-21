0e52b553465cdbadaafec0791da4e46043d009ff
mmrzax <mmrzax@gmail.com>
2022-07-21 16:13:43 +0430

    feat: idk

--- dot.sh
+++ dot.sh
@@ -1,5 +1,6 @@
 cp -r ~/.config/alacritty ~/Documents/dotfiles
 cp -r ~/.config/htop ~/Documents/dotfiles
+cp -r ~/.config/gotop ~/Documents/dotfiles
 cp -r ~/.config/i3 ~/Documents/dotfiles
 cp -r ~/.config/i3status ~/Documents/dotfiles
 cp -r ~/.config/nvim ~/Documents/dotfiles
